cbd85467dc29d41314d33919c5103b4fa7f6c0a6
kenrick95 <kenrick95@gmail.com>
2014-12-24 14:26:27 +0700

    +GUI input

--- COTS/src/v1/COTSGenerator.java
+++ COTS/src/v1/COTSGenerator.java
@@ -2,47 +2,86 @@ package v1;
 
 import java.util.Scanner;
 
+import javax.swing.*;
+
+import java.awt.*;
+
 /**
  * The main application class of COTS Diagram Automatic Generator.
  * 
- * @author Edwin Candinegara
+ * @author Edwin Candinegara & Kenrick
  * @version 1.0
  *
  */
 
-public class COTSGenerator 
+public class COTSGenerator extends JFrame
 {
+	private JTextArea tracker;
+	
+	//Using a standard Java icon
+    private Icon optionIcon = UIManager.getIcon("FileView.computerIcon");
+    
+    
 	public static void main (String[] args)
 	{
-		String s;
-		Scanner scan = new Scanner (System.in);
-		
-		System.out.println("Please note that current version of COTS Diagram Generator has a format for a given expression:");
-		System.out.println("1. For each 2 operands, it has to be bounded by opening and closing parenthesis");
-		System.out.println("2. Do not put any whitespace in between");
-		System.out.println("Example:");
-		System.out.println("((1->2)||3)");
-		System.out.println("((1->2)||(3->4))");
-		System.out.println("(((1->2)->3)->4)");
-		System.out.println("((1->((2->3)||(4->5)))->6)");
-		System.out.println("");
-		
-		System.out.print("Enter an expression: ");
-		s = scan.next();
-		
-		while (s.length() == 0) {
-			System.out.println("Wrong Expression!");
-			System.out.print("Enter a valid expression: ");
-			s = scan.next();
-		}
-		
-		Parser parser = new Parser(s);
-		parser.parseExpression();
-		
-		DiagramGenerator diagramGenerator = new DiagramGenerator();
-		diagramGenerator.constructLines();
-		diagramGenerator.generateDiagram();
-		
-		scan.close();
+		//Use the event dispatch thread for Swing components
+	     EventQueue.invokeLater(new Runnable()
+	     {
+	         public void run()
+	         {
+	             //create GUI frame
+	             new COTSGenerator().setVisible(true);          
+	         }
+	     });
 	}
+	public COTSGenerator()
+    {
+        //make sure the program exits when the frame closes
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setTitle("COTS Diagram Generator");
+        setSize(500,300);
+        
+        //This will center the JFrame in the middle of the screen
+        setLocationRelativeTo(null);
+        
+        //Using JTextArea to show clicks and responses
+        tracker = new JTextArea("COTS Diagram Generator\n");
+        add(tracker);
+        setVisible(true);
+        
+        //Input dialog with a text field
+        String input =  JOptionPane.showInputDialog(this, "Please note that current version of COTS Diagram Generator has a format for a given expression:\n"
+        		+ "1. For each 2 operands, it has to be bounded by opening and closing parenthesis\n"
+        		+ "2. Do not put any whitespace in between\n"
+        		+ "Example:\n"
+        		+ "((1->2)||3)\n"
+        		+ "((1->2)||(3->4))\n"
+        		+ "(((1->2)->3)->4)\n"
+        		+ "((1->((2->3)||(4->5)))->6)\n"
+        		+ "\n",
+        		"((1->((2->3)||(4->5)))->6)");
+        TrackResponse(input);
+    }
+    
+    //Append the picked choice to the tracker JTextArea
+    public void TrackResponse(String response)
+    {
+        //showInputDialog method returns null if the dialog is exited
+        //without an option being chosen
+        if (response == null)
+        {
+            tracker.append("You closed the dialog without any input\n");
+        }
+        else
+        {
+            tracker.append("Input received: " + response + "\n");
+            
+            Parser parser = new Parser(response);
+    		parser.parseExpression();
+    		
+    		DiagramGenerator diagramGenerator = new DiagramGenerator();
+    		diagramGenerator.constructLines();
+    		diagramGenerator.generateDiagram();
+        }
+    }
 }
